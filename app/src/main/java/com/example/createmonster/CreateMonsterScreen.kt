5f5cea30d6926bab3d1dbf09f2231deed61efafd
Noukta <ahmed.noukta@gmail.com>
2023-05-07 12:50:45 +0100

    add (privacy policy) in the top app bar. replace share and (privacy policy) logique to utils package.

--- app/src/main/java/com/example/createmonster/CreateMonsterScreen.kt
+++ app/src/main/java/com/example/createmonster/CreateMonsterScreen.kt
@@ -1,13 +1,12 @@
 package com.example.createmonster
 
-import android.content.ClipData
 import android.content.Context
-import android.content.Intent
-import android.graphics.Bitmap
-import android.net.Uri
-import android.os.Environment
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -16,8 +15,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
-import androidx.core.content.FileProvider
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -27,9 +30,10 @@ import com.example.createmonster.ui.CreateMonsterViewModel
 import com.example.createmonster.ui.CreateScreen
 import com.example.createmonster.ui.EndScreen
 import com.example.createmonster.ui.StartScreen
+import com.example.createmonster.utils.openPrivacyPolicy
+import com.example.createmonster.utils.saveToInternalStorage
+import com.example.createmonster.utils.shareImageUri
 import com.smarttoolfactory.screenshot.rememberScreenshotState
-import java.io.File
-import java.io.FileOutputStream
 
 enum class CreateMonsterScreen {
     Start,
@@ -37,46 +41,42 @@ enum class CreateMonsterScreen {
     End
 }
 
-/*@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
 fun CreateMonsterAppBar(
-    currentScreen: CreateMonsterScreen,
+    context: Context,
     canNavigateBack: Boolean,
-    navigateUp: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     TopAppBar(
-        title = { Text(stringResource(currentScreen.title)) },
+        title = {},
         modifier = modifier,
-        navigationIcon = {
-            if (canNavigateBack) {
-                IconButton(onClick = navigateUp) {
+        actions = {
+            if (!canNavigateBack) {
+                IconButton(onClick = { openPrivacyPolicy(context) }) {
                     Icon(
-                        imageVector = Icons.Filled.ArrowBack,
-                        contentDescription = stringResource(R.string.back_button)
+                        imageVector = ImageVector.vectorResource(id = R.drawable.baseline_policy_24),
+                        contentDescription = stringResource(R.string.privacy_policy),
+                        tint = Color.Unspecified
                     )
                 }
             }
         }
     )
-}*/
+}
 
 @Composable
 fun CreateMonsterApp(modifier: Modifier = Modifier, viewModel: CreateMonsterViewModel = viewModel()){
     val navController = rememberNavController()
-
-    /*val backStackEntry by navController.currentBackStackEntryAsState()
-    val currentScreen = CreateMonsterScreen.valueOf(
-        backStackEntry?.destination?.route ?: CreateMonsterScreen.Start.name
-    )*/
+    val context = LocalContext.current
 
     Scaffold(
-        /*topBar = {
+        topBar = {
             CreateMonsterAppBar(
-                currentScreen = currentScreen,
-                canNavigateBack = navController.previousBackStackEntry != null,
-                navigateUp = { navController.navigateUp() }
+                context = context,
+                canNavigateBack = navController.previousBackStackEntry != null
             )
-        }*/
+        }
     ) { innerPadding ->
         val uiState by viewModel.uiState.collectAsState()
         NavHost(
@@ -106,8 +106,6 @@ fun CreateMonsterApp(modifier: Modifier = Modifier, viewModel: CreateMonsterView
             }
             composable(route = CreateMonsterScreen.End.name) {
                 val screenshotState = rememberScreenshotState()
-                val context = LocalContext.current
-
                 var showDialog by remember { mutableStateOf(false) }
                 // Show dialog only when ImageResult is success or error
                 LaunchedEffect(key1 = screenshotState.bitmap){
@@ -134,35 +132,16 @@ fun CreateMonsterApp(modifier: Modifier = Modifier, viewModel: CreateMonsterView
     }
 }
 
-private fun Bitmap.saveToInternalStorage(context: Context, fileName: String): Uri {
-    val imagesDir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-    val imageFile = File(imagesDir, fileName)
-    val outputStream = FileOutputStream(imageFile)
-    this.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
-    outputStream.flush()
-    outputStream.close()
-    return FileProvider.getUriForFile(
-        context,
-        "${BuildConfig.APPLICATION_ID}.provider",
-        imageFile
-    )
-}
-
-private fun shareImageUri(context: Context, uri: Uri, message: String) {
-    val intent = Intent(Intent.ACTION_SEND).apply {
-        clipData = ClipData.newRawUri(null, uri)
-        putExtra(Intent.EXTRA_STREAM, uri)
-        putExtra(Intent.EXTRA_TEXT, message)
-        addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
-        type = "image/png"
-    }
-    context.startActivity(Intent.createChooser(intent, "Share Monster"))
-}
-
 private fun remakeNewMonster(
     viewModel: CreateMonsterViewModel,
     navController: NavHostController
 ) {
     viewModel.resetMonster()
     navController.popBackStack(CreateMonsterScreen.Start.name, inclusive = false)
+}
+
+@Preview
+@Composable
+fun CreateMonsterAppPreview(){
+    CreateMonsterApp()
 }
---
+++ app/src/main/java/com/example/createmonster/utils/Util.kt
@@ -0,0 +1,44 @@
+package com.example.createmonster.utils
+
+import android.content.ClipData
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Environment
+import androidx.core.content.FileProvider
+import com.example.createmonster.BuildConfig
+import java.io.File
+import java.io.FileOutputStream
+
+fun Bitmap.saveToInternalStorage(context: Context, fileName: String): Uri {
+    val imagesDir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+    val imageFile = File(imagesDir, fileName)
+    val outputStream = FileOutputStream(imageFile)
+    this.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+    outputStream.flush()
+    outputStream.close()
+    return FileProvider.getUriForFile(
+        context,
+        "${BuildConfig.APPLICATION_ID}.provider",
+        imageFile
+    )
+}
+
+fun shareImageUri(context: Context, uri: Uri, message: String) {
+    val intent = Intent(Intent.ACTION_SEND).apply {
+        clipData = ClipData.newRawUri(null, uri)
+        putExtra(Intent.EXTRA_STREAM, uri)
+        putExtra(Intent.EXTRA_TEXT, message)
+        addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+        type = "image/png"
+    }
+    context.startActivity(Intent.createChooser(intent, "Share Monster"))
+}
+
+fun openPrivacyPolicy(context: Context) {
+    val url = "https://wkeystudio.blogspot.com/2023/04/privacy-policy.html"
+    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+    context.startActivity(intent)
+}
