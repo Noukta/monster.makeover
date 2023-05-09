3d419a81d8c5be7a111b0df2f15082c9c7cc02b5
Noukta <ahmed.noukta@gmail.com>
2023-05-09 17:32:37 +0100

    Hid topBar when currentDestination is not the StartScreen

--- app/src/main/java/com/example/createmonster/CreateMonsterScreen.kt
+++ app/src/main/java/com/example/createmonster/CreateMonsterScreen.kt
@@ -45,38 +45,38 @@ enum class CreateMonsterScreen {
 @Composable
 fun CreateMonsterAppBar(
     context: Context,
-    canNavigateBack: Boolean,
     modifier: Modifier = Modifier
 ) {
     TopAppBar(
         title = {},
         modifier = modifier,
         actions = {
-            if (!canNavigateBack) {
-                IconButton(onClick = { openPrivacyPolicy(context) }) {
-                    Icon(
-                        imageVector = ImageVector.vectorResource(id = R.drawable.baseline_policy_24),
-                        contentDescription = stringResource(R.string.privacy_policy),
-                        tint = Color.Unspecified
-                    )
-                }
+            IconButton(onClick = { openPrivacyPolicy(context) }) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.baseline_policy_24),
+                    contentDescription = stringResource(R.string.privacy_policy),
+                    tint = Color.Unspecified
+                )
             }
         }
     )
 }
 
 @Composable
-fun CreateMonsterApp(modifier: Modifier = Modifier, viewModel: CreateMonsterViewModel = viewModel()){
+fun CreateMonsterApp(
+    modifier: Modifier = Modifier,
+    viewModel: CreateMonsterViewModel = viewModel()
+) {
     val navController = rememberNavController()
     val context = LocalContext.current
-    var canNavigateBack by remember {mutableStateOf(false)}
 
     Scaffold(
         topBar = {
-            CreateMonsterAppBar(
-                context = context,
-                canNavigateBack = canNavigateBack
-            )
+            if (navController.currentDestination?.route == CreateMonsterScreen.Start.name) {
+                CreateMonsterAppBar(
+                    context = context
+                )
+            }
         }
     ) { innerPadding ->
         val uiState by viewModel.uiState.collectAsState()
@@ -84,35 +84,34 @@ fun CreateMonsterApp(modifier: Modifier = Modifier, viewModel: CreateMonsterView
             navController = navController,
             startDestination = CreateMonsterScreen.Start.name,
             modifier = modifier.padding(innerPadding)
-        ){
+        ) {
             composable(route = CreateMonsterScreen.Start.name) {
                 StartScreen(
                     onNextButtonClicked = {
                         navController.navigate(CreateMonsterScreen.Create.name)
-                        canNavigateBack = true
                     }
                 )
             }
             composable(route = CreateMonsterScreen.Create.name) {
                 CreateScreen(
                     monsterUiState = uiState,
-                    onMonsterHeadChanged = {id -> viewModel.updateMonsterHead(id)},
-                    onMonsterEyeChanged = {id -> viewModel.updateMonsterEye(id)},
-                    onMonsterMouthChanged = {id -> viewModel.updateMonsterMouth(id)},
-                    onMonsterAccChanged = {id -> viewModel.updateMonsterAcc(id)},
-                    onMonsterBodyChanged = {id -> viewModel.updateMonsterBody(id)},
-                    onEyePositionChanged = {viewModel.updateEyePosition(it)},
-                    onMouthPositionChanged = {viewModel.updateMouthPosition(it)},
-                    onAccPositionChanged = {viewModel.updateAccPosition(it)},
-                    onDoneButtonClicked = {navController.navigate(CreateMonsterScreen.End.name) }
-                    )
+                    onMonsterHeadChanged = { id -> viewModel.updateMonsterHead(id) },
+                    onMonsterEyeChanged = { id -> viewModel.updateMonsterEye(id) },
+                    onMonsterMouthChanged = { id -> viewModel.updateMonsterMouth(id) },
+                    onMonsterAccChanged = { id -> viewModel.updateMonsterAcc(id) },
+                    onMonsterBodyChanged = { id -> viewModel.updateMonsterBody(id) },
+                    onEyePositionChanged = { viewModel.updateEyePosition(it) },
+                    onMouthPositionChanged = { viewModel.updateMouthPosition(it) },
+                    onAccPositionChanged = { viewModel.updateAccPosition(it) },
+                    onDoneButtonClicked = { navController.navigate(CreateMonsterScreen.End.name) }
+                )
 
             }
             composable(route = CreateMonsterScreen.End.name) {
                 val screenshotState = rememberScreenshotState()
                 var showDialog by remember { mutableStateOf(false) }
                 // Show dialog only when ImageResult is success or error
-                LaunchedEffect(key1 = screenshotState.bitmap){
+                LaunchedEffect(key1 = screenshotState.bitmap) {
                     screenshotState.bitmap?.let { showDialog = true }
                 }
 
@@ -121,11 +120,10 @@ fun CreateMonsterApp(modifier: Modifier = Modifier, viewModel: CreateMonsterView
                     screenshotState = screenshotState,
                     onRemakeButtonClicked = {
                         remakeNewMonster(viewModel, navController)
-                        canNavigateBack = false
                     }
                 ) {
                     screenshotState.capture()
-                    if(showDialog) {
+                    if (showDialog) {
                         screenshotState.bitmap?.let {
                             val uri = it.saveToInternalStorage(context, "monster.png")
                             val message =
@@ -137,6 +135,7 @@ fun CreateMonsterApp(modifier: Modifier = Modifier, viewModel: CreateMonsterView
             }
         }
     }
+
 }
 
 private fun remakeNewMonster(
@@ -149,6 +148,6 @@ private fun remakeNewMonster(
 
 @Preview
 @Composable
-fun CreateMonsterAppPreview(){
+fun CreateMonsterAppPreview() {
     CreateMonsterApp()
 }
