99ad13e4422b0fd431ebdb1f6cf33646e48d9ea3
Noukta <ahmed.noukta@gmail.com>
2023-06-02 13:09:52 +0100

    add: admob ids for each app in comments.

--- app/src/main/java/com/monster/makeover/ads/admob/AdmobConstant.kt
+++ app/src/main/java/com/monster/makeover/ads/admob/AdmobConstant.kt
@@ -1,10 +1,9 @@
 package com.monster.makeover.ads.admob
 
 
-//real ad ids
+//monster.makeover ad ids
 /*
 object AdmobConstant {
-    const val APP_ID = "ca-app-pub-9919447440379444~4494133729"
     const val APP_OPEN = "ca-app-pub-9919447440379444/3907864261"
     const val INTERSTITIAL_START_CREATE = "ca-app-pub-9919447440379444/9471128238"
     const val INTERSTITIAL_CREATE_END = "ca-app-pub-9919447440379444/8458301601"
@@ -12,6 +11,20 @@ object AdmobConstant {
     const val NATIVE = ""
     const val BANNER_START = "ca-app-pub-9919447440379444/6945091144"
     const val BANNER_END = "ca-app-pub-9919447440379444/5915026602"
+    const val COUNTER_TIME = 3L
+}
+*/
+
+//mix.monster ad ids
+/*
+object AdmobConstant {
+    const val APP_OPEN = "ca-app-pub-9919447440379444/6848710615"
+    const val INTERSTITIAL_START_CREATE = "ca-app-pub-9919447440379444/5152485562"
+    const val INTERSTITIAL_CREATE_END = "ca-app-pub-9919447440379444/7258902013"
+    const val REWARDED_COINS = "ca-app-pub-9919447440379444/1499841705"
+    const val NATIVE = ""
+    const val BANNER_START = "ca-app-pub-9919447440379444/8571983683"
+    const val BANNER_END = "ca-app-pub-9919447440379444/2198147027"
     const val COUNTER_TIME = 3L
 }
 */
